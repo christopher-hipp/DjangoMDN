b0cc01317ff7ffe07928ae37a65bed15d49a51b4
Christopher Hipp Kuut <79537682+christopher-hipp@users.noreply.github.com>
2021-12-03 20:44:06 +0200

    Update admin page

--- v2L_website/catalog/admin.py
+++ v2L_website/catalog/admin.py
@@ -12,9 +12,15 @@ admin.site.register(Language)
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ("last_name", "first_name", "date_of_birth", "date_of_death")
 
+    fields = ["first_name", "last_name", ("date_of_birth", "date_of_death")]
+
 
 # Register the admin class with the associated model
 admin.site.register(Author, AuthorAdmin)
+
+
+class BookInstanceInline(admin.TabularInline):
+    model = BookInstance
 
 
 # This decorator does the same this as admin.site.register()
@@ -22,7 +28,18 @@ admin.site.register(Author, AuthorAdmin)
 class BookAdmin(admin.ModelAdmin):
     list_display = ("title", "author", "display_genre")
 
+    inlines = [BookInstanceInline]
+
 
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
     list_filter = ("status", "due_back")
+
+    fieldsets = (
+        (None, {
+            "fields": ("book", "imprint", "id")
+        }),
+        ("Availability", {
+            "fields": ("status", "due_back")
+        }),
+    )
